3542ed522da384ff95eec9cfb24aff9a4c755dd4
John <dongyongkim8@gmail.com>
2023-09-25 23:56:57 -0400

    get bearer token function works trying to add token to storage for auth

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
 const express = require('express');
-const cors = require('cors');
+const http = require('http');
+const querystring = require('querystring');
+const btoa = require('btoa');
+const axios = require('axios');
+const cors = require ('cors')
 
-const { getBearerToken } = require('./spotify');
+const { recetlyPlayedTracks } = require('./spotify');
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:8080',
-};
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+// };
 
-app.use(cors(corsOptions));
+const tokenEndpoint = 'https://accounts.spotify.com/api/token';
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+app.use(express.json()); // For parsing JSON requests
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+let accessToken = null;
+
+app.use(async (req, res, next) => {
+  if (!accessToken) {
+    try {
+      const data = new URLSearchParams();
+      data.append('grant_type', 'client_credentials');
+      const authString = `${req.body.clientSecret}:${req.body.clientId}`;
+      const base64AuthString = btoa(authString); 
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic ' + base64AuthString,
+        },
+      };
+
+      const response = await axios.post(tokenEndpoint, data, config);
+console.log(response)
+      if (response.status === 200) {
+        accessToken = response.data.access_token;
+
+        // Set a timer to clear the access token after it expires
+        const expiresInMilliseconds = response.data.expires_in * 1000;
+        setTimeout(() => {
+          accessToken = null;
+        }, expiresInMilliseconds);
+      } else {
+        throw new Error('Failed to obtain access token');
+      }
+    } catch (error) {
+      console.error('Error fetching access token:', error.message);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  req.accessToken = accessToken;
+  next();
 });
 
 const port = process.env.PORT || 3000;
@@ -21,6 +65,12 @@ app.listen(port, () => {
 });
 
 
-app.post('/get-spotify-token', async (req, res) => {
-  await getBearerToken(req, res)
+app.post('/recent-tracks', async (req, res) => {
+  const token = req.accessToken;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  await recetlyPlayedTracks(req, res)
 })
--- spotify.js
+++ spotify.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-const btoa = require('btoa');
+
+const tokenEndpoint = 'https://accounts.spotify.com/api/token';
 
 async function getBearerToken(req, res) {
   try {
     const data = new URLSearchParams();
     data.append('grant_type', 'client_credentials');
-console.log(req.body)
-    const authString = `1afde426b48347c7baacc526df092900:953ea612f6e14c5d86072707d8dc4bba`;
+    const authString = `${req.body.clientSecret}:${req.body.clientId}`;
     const base64AuthString = btoa(authString); 
 
     const config = {
@@ -15,19 +15,37 @@ console.log(req.body)
         Authorization: 'Basic ' + base64AuthString,
       },
     };
+    console.log(data)
 
-    const response = await axios.post(
-      'https://accounts.spotify.com/api/token',
-      data,
-      config
-    );
-
-    const accessToken = response.data.access_token;
-    res.json({ access_token: accessToken });
+    const response = await axios.post(tokenEndpoint, data, config);
+    if (response.status === 200) {
+      const accessToken = response.data.access_token;
+      res.json({ access_token: accessToken });
+    } else {
+      throw new Error('Failed to obtain access token');
+    }
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error fetching access token:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
 
+async function recetlyPlayedTracks(req, res) {
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + req.accessToken,
+      'Content-Type': 'application/json'
+    }
+  };
+
+  return axios.get('https://api.spotify.com/v1/me/player/recently-played', config)
+    .then((response) => {
+      res.json({ recent_tracks: response.data });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
+}
+
+module.exports.recetlyPlayedTracks = recetlyPlayedTracks;
 module.exports.getBearerToken = getBearerToken;
